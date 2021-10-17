25e709d75268bf1ad38cd5be64c210775af3b23d
PRITESH CHOGALE <priteshchogale@gmail.com>
2021-10-17 11:37:55 +0530

    tests

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,46 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    public void testTwoNumbers(){
+        assertEquals(3, Calculator.add("1,2"));
+    }
+
+    @Test
+     public void testThreeNumbers(){
+        assertEquals(6, Calculator.add("1,2,3"));
+     }
+
+     @Test
+     public void testNewLine(){
+        assertEquals(6, Calculator.add("1\n2,3"));
+     }
+
+     @Test
+     public void testNegativeNumver(){
+        try {
+            Calculator.add("-1,2");
+        }
+        catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(), "Negatives not allowed: -1");
+        }
+
+        try {
+            Calculator.add("2,-4,3,-5");
+        }
+        catch (IllegalArgumentException e){
+            assertEquals(e.getMessage(), "Negatives not allowed: -4,-5");
+        }
+     }
+
+     @Test
+     public void testOverThousand(){
+        assertEquals(2, Calculator.add("1000,2"));
+     }
+
+     @Test
+     public void testOtherDelimiter(){
+        assertEquals(3, Calculator.add("//;\n1;2"));
+     }
 }
